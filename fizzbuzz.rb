4a0677642a1148ec291a22d4a461501bb9826829
RaffiSapire <raphaelasapire@gmail.com>
2016-02-04 12:22:14 -0500

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+
+def fizzbuzz(x)
+
+  if (x % 3 == 0) && (x % 5== 0)
+    "FizzBuzz" 
+  elsif x % 5 == 0
+    "Buzz"
+  elsif x % 3 == 0
+    "Fizz"
+end
+end
+
+fizzbuzz(3)
+fizzbuzz(15)
+fizzbuzz(5)
