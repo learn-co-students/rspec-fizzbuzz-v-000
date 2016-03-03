3c711a73e806c3a7741b89fccc0dd8fad9c6adf3
Hugo M <h.murillo21@yahoo.com>
2016-03-03 04:08:29 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
 if int % 15 == 0
-  "FizzBuzz"
+  puts "FizzBuzz"
   elsif int % 3 == 0
-  "Fizz"
-  elsif int % 5 == 0
-  "Buzz"
-  else
-  puts "nil"
+  puts "Fizz"
+  else int % 5 == 0
+  puts "Buzz"
+#  else
+#  puts "nil"
 end
 end
