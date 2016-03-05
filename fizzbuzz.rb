63d32b49e9cbce3ba552605b3783930d1661c39e
emanovic7 <e_owusu@live.com>
2016-03-05 04:01:16 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    return "Fizz"
+  if int % 3 == 0 && int % 5 ==0
+    return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
   else
     return nil
   end
