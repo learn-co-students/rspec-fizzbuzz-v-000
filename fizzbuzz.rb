b4c139679fe4ae549ba20b3fcf4ba763110af79e
bhabig <rfhab2@gmail.com>
2016-04-06 16:29:22 +0000

    nitrous-backup at Wed Apr  6 16:29:21 UTC 2016

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 15 == 0
+  if int % 5 == 0 && int % 3 == 0
     "FizzBuzz"
-  elsif int % 3 == 0
+  elsif int % 3 == 0 && int % 5 != 0
     "Fizz"
-  elsif int % 5 == 0
+  elsif int % 5 == 0 && int % 3 != 0
     "Buzz"
   end
 end
