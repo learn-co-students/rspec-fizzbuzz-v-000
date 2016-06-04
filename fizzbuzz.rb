9d3e39e6be5b99e5116e98a0978fc7fa9c3be26a
Scott Wakeman <swakeman@ca.rr.com>
2016-06-04 15:49:37 -0700

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,30 +1,49 @@
 #!/usr/bin/env ruby -wKU
-require_relative './spec_helper.rb'
-require_relative '../fizzbuzz_spec.rb'
+describe "fizzbuzz" do
+  it 'returns "fizz" when the number is divisible by 3' do
+    fizz_3 = fizzbuzz(3)
+
+    expect(fizz_3).to eq("fizz")
+  end
+end
+
+describe "fizzbuzz" do
+  it 'returns "buzz" when the number is divisible by 5'do
+    fizz_5 = fizzbuzz(5)
+
+    expect(fizz_5).to eq("buzz")
+  end
+end
+
+describe "fizzbuzz" do
+it 'returns "fizzbuzz" when the number is divisible by 3 and 5'do
+    fizz_15 = fizzbuzz(15)
+
+    expect (fizz_15).to eq("fizzbuzz")
+  end
+end
+
+
+describe "fizzbuzz" do
+  it 'returns nil when the number is not divisible by 3 or 5'do
+    fizz_4 = fizzbuzz(4)
+
+    expect(fizz_4). to eq(nil)
+  end
+end
 
 def fizzbuzz
 end
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-  end
-end
-      
-def fizzbuzz(int)
-  if int % 5 == 0 # if the number int is divisible by 5
-    "Buzz" # Go Buzz
-  end
-end
-
-def fizzbuzz(int)
   if int % 15 == 0 # if the number int is divisible by 3 and 5
-    "Fizzbuzz"
-  end
-end
+    "fizzbuzz" # Go Fizzbuzz
 
-def fizzbuzz(int)
-  if int % 4 == 0 # if the number int is not divisible by 3 or 5
-    "nil"
+  elsif int % 5 == 0 # if the number int is divisible by 5
+    "buzz" # Go buzz
+
+  elsif int % 3 == 0 # if the number int is divisible by 3
+  "fizz" # Go fizz
+
   end
 end
