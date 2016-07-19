c4a096b1da5a4d6955bd2fea9579647fe2ce0265
Leonard Bernardo <leonard.bernardo@gmail.com>
2016-07-19 23:06:08 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,12 @@
 def fizzbuzz (int)
-if (int % 3) == 0
-   "Fizz"
+if(int % 3 == 0)  && (int % 5 == 0)
+   "FizzBuzz"
  elsif (int % 5 == 0)
     "Buzz"
-  elsif (int % 3 == 0) &&  (int % 5 == 0)
-   "FizzBuzz"
+ elsif (int % 3) == 0
+    "Fizz"
+  end
 end
-end
+
+
+##    elsif(int % 3 == 0)  && (int % 5 == 0)
