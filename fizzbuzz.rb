14de366456f66432192f6159193d8fd2429f1f3f
Elias Ventura <eliasventura23@gmail.com>
2016-10-27 22:14:45 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
+def fizzbuzz(number)
+  if number/3 == 0 && number/5 == 0
     "FizzBuzz"
-  elsif int % 3 == 0
+  elsif number/3 == 0
     "Fizz"
-  elsif int % 5 == 0
+  elsif number/5 == 0
     "Buzz"
   end
 end
