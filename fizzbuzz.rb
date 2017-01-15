987c7a8e548b073d4e5696f8d8757b02ccf3123d
Katie Smith <catesmith0@gmail.com>
2017-01-15 23:48:42 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,6 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  #elsif (int % 3 == 0) && (int % 5 ==0)
-  #  return "FizzBuzz"
-  #else (int !% 3 ==0) || (int !% 5 == 0)
-    #return false
   end
 end
 
