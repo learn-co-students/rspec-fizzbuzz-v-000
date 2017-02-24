59b39f2d0bf8e562b30e7a3250794004d8c37c26
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-02-24 03:15:06 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+
+def fizzbuzz (int)
+if int % 3 == 0
+  "Fizz"
+end
+  end
+
+def fizzbuzz (int)
+  if int % 5 == 0
+    "Buzz"
+  end
+    end
+
+  def fizzbuzz (int)
+    if int % 15 == 0
+      "FizzBuzz"
+    end
+      end
