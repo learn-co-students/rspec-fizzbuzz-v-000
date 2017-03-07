203ffa980690617bf4735d6d6edf8844f5c12971
Julia Thompson <thompson.julia@gmail.com>
2017-03-07 12:33:25 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(num)
   str = ""
-  if (num % 3 == 0 && num % 5 == 0)
-    return "FizzBuzz"
+  if (num % 3 == 0 && num % 5 == 0) # don't need parentheses
+    return "FizzBuzz" # can just say "FizzBuzz - no need for return keyword"
   elsif (num % 3 == 0)
     return "Fizz"
   elsif (num % 5 == 0)
     return "Buzz"
   else
-    return nil
+    return nil #don't need this: will return nil by default
   end
 end
