5ae51e9e1046177b30c1944179967673f327af12
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-03-19 18:35:01 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if (int % 3 == 0) && (int % 5 != 0)
-    "Fizz"
-  elsif (int % 5 == 0) && (int % 3 != 0)
-    "Buzz"
-  elsif (int % 3 == 0) && (int % 5 == 0)
+  if (int % 3 == 0) && (int % 5 == 0)
     "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
   else
-    nil      
-  end  
-end  
+    nil
+  end
+end
