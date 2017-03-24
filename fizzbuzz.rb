755200f11faf5f38623216f924a240b1ab29860f
jose cobos <luiscobosj@gmail.com>
2017-03-24 19:11:18 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+# jose cobos Mar 24 2017
+
+
+def fizzbuzz(int)
+
+  if int % 3 == 0 && int % 5 != 0
+   "Fizz"
+  
+else if int % 5 == 0 && int % 3 != 0
+   "Buzz"
+  
+else if int % 3 == 0 && int % 5 == 0
+   "FizzBuzz"
+  
+end
+end
+end
+end
