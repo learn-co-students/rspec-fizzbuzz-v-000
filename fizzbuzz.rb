c3369ce577625edc31b5efb259b3a3544c20ae28
Harvie Brown <kurohyo70@gmail.com>
2017-04-24 15:06:52 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(int)
+if int % 15 == 0 # If number int is divisible by 15
+  "FizzBuzz"
+  elsif int % 3 == 0 # If the number int is divisible by 3
+    "Fizz" # Go Fizz!!
+  elsif int % 5 == 0 # If the number int is divisible by 5
+      "Buzz" # Go Buzz!!
+  #elsif int % 15 == 0 # If number int is divisible by 15
+      #"FizzBuzz" # Go FizzBuzz!!
+  end
+end
