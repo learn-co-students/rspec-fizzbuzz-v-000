38661ae671b0f266de1624d02fb208b034f708aa
Harvie Brown <kurohyo70@gmail.com>
2017-04-24 15:43:14 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,5 @@ if int % 15 == 0 # If number int is divisible by 15
     "Fizz" # Go Fizz!!
   elsif int % 5 == 0 # If the number int is divisible by 5
       "Buzz" # Go Buzz!!
-  #elsif int % 15 == 0 # If number int is divisible by 15
-      #"FizzBuzz" # Go FizzBuzz!!
-  end
+    end
 end
