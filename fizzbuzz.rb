f1024e59b0c1510829efc6706b4b34f5232b6e08
Jennifer Sung <jennifer.sung@colorado.edu>
2017-04-27 02:40:03 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+def fizzbuzz(int)
+
+if int % 3 == 0 && int % 5 == 0
+  "FizzBuzz"
+elsif int % 5 == 0
+    "Buzz"
+  
+  elsif int % 3 == 0
+      "Fizz"
+  else int % 4 == 0
+      nil
+    end
+end
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+fizzbuzz(4)
