a8e6b13eca0feff9871cac65b6ae7d9d76e6e47b
gb23 <gregbenj@hotmail.com>
2017-05-03 15:59:22 -0700

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,11 @@
 
 def fizzbuzz(int)
 #binding.pry
-if int % 3 == 0 && int % 5 == 0
-  "FizzBuzz"
-elsif int % 3 == 0
-  "Fizz"
-elsif int % 5 == 0
-  "Buzz"
-end
-
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
 end
