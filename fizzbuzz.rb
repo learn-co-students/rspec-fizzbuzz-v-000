f36d62b961ac2ee69e76bd90ed45bdaa3210dd71
Chidinma Eze <ceze1990@gmail.com>
2017-05-04 22:24:25 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(int)
+if int % 3 == 0 && int % 5 == 0
+  puts "FizzBuzz"
+  fizz_15 = "FizzBuzz"
+elsif int % 3 == 0
+  puts "Fizz"
+  fizz_3 = "Fizz"
+elsif int % 5 == 0
+puts "Buzz"
+fizz_5 = "Buzz"
+else
+  puts nil
+  fizz_4 = nil
+end
+end
