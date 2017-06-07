ce7dbe8be9993073a6327e639d3093092a03df4b
Robert Laws <roblaws@me.com>
2017-06-07 18:23:28 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(int)
+  if int % 3 == 0
+    if int % 3 == 0 && int % 5 == 0
+      "FizzBuzz"
+    else
+      "Fizz"
+    end
+  elsif int % 5 == 0
+    if int % 3 == 0 && int % 5 == 0
+      "FizzBuzz"
+    else
+      "Buzz"
+    end
+  end
+end
