0c7c298c21252898e075fc1a244642ec2a6d9252
Kaitlin Kelly <kk504408@gmail.com>
2017-07-21 18:33:04 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(int)
+  if int % 15 == 0
+    fizz_15 = "FizzBuzz"
+  elsif int % 3 == 0
+    fizz_3 =  "Fizz"
+  elsif int % 5 == 0
+    fizz_5 = "Buzz"
+  else
+    fizz_4 = nil
+  end
+end
