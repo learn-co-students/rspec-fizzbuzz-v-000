8b8a1e8cd760a3bbcf48f9bb6a441df89e7b313c
Johan Esteves <jsesteves91@gmail.com>
2017-08-11 01:29:33 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(int)
+
+div3 = int % 3
+div5 = int % 5
+
+  if (div3 == 0 && div5 == 0)
+     "FizzBuzz"
+  elsif div3 == 0
+     "Fizz"
+  elsif div5 == 0
+     "Buzz"
+  else
+
+  end
+
+end
