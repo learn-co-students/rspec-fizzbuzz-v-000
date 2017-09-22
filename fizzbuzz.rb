a714fb1943a4edb37e9a8c4dacf5f5a136095012
Yana Ustsinovich <yustinovich@icloud.com>
2017-09-22 05:41:40 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,21 @@
+def fizzbuzz(play)
+ if play % 3 == 0 && play % 5 == 0
+   "FizzBuzz"
+ elsif play % 5 == 0
+   "Buzz"
+ elsif play % 3 == 0
+ "Fizz"
+ end
+end
+=begin elsif play % 3 == 0
+    print "Fizz"
+  elsif play % 5 == 0
+    print "Buzz"
+  else
+    puts nil
+ end
+end
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+=end
