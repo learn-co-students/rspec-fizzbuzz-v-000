27788dc7a4363f64fc92aaf8075728522dfc3db6
Ron Sala <ron.sala@gmail.com>
2017-10-14 04:20:47 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,13 @@
 def fizzbuzz(int)
 
-  if (int % 3 == 0) && (int % 5 == 0)
+  if int % 15 == 0
     "FizzBuzz"
-  end
-
   elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
+  elsif int % 5 == 0 # if the number int is divisible by 5
+    "Buzz" # Go fizz
+  else
+    return nil
   end
-
-#  else
-#    return nil
-#  end
 
 end
