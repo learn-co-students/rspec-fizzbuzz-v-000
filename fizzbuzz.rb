1afeda0f9842ea734122d39741d2d6b9cfd5cde9
Brent  Ransom <sadumedusa@gmail.com>
2017-10-16 16:38:02 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int == 15
   return "FizzBuzz"
-elsif int == 5
+elsif int % 5 == 0
   return "Buzz"
-elsif int == 3
+elsif int % 3 == 0
   return "Fizz"
 else int == 4
   return nil
