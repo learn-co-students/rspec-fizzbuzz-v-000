5c9c02731f6b01d85073d8e25d68ecc4093fa1fe
Sara Schwarz <saramschwarz@gmail.com>
2017-10-18 22:12:28 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,16 @@
-def fizzbuzz
-fizzbuzz(3) == "Fizz"
+def fizzbuzz(int)
+
+  end
+
+if int % 3 == 0
+  then puts  "Fizz"
+end
+
+if int % 5 == 0
+  then puts "Buzz"
+end
+
+if int % 5 == 0 && i % 3 == 0
+  then puts "FizzBuzz"
+end
 end
