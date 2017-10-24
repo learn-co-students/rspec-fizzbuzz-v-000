30e43ea1bf3b4b66207bf20127162242a05907a5
mstmari <michael.landrum.st.marie@gmail.com>
2017-10-24 01:48:33 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,31 @@
+def fizzbuzz(int)
+  if int % 15 == 0
+    "FizzBuzz"
+ elsif int % 5 == 0
+   "Buzz"
+ elsif int % 3 == 0
+"Fizz"
+ elsif int % 4 == 0
+     nil
+    end
+  end
+
+
+
+#def fizzbuzz (int)
+#if int % 5 == 0
+#  "Buzz"
+#  end
+#end
+
+#def fizzbuzz (int)
+#  if int % 15 == 0
+#    "FizzBuzz"
+#  end
+#end
+
+#  def fizzbuzz (int)
+#if int % 4 == 0
+#  nil
+#  end
+#end
