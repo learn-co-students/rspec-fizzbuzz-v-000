55dc4da682b02636e5fe674f3acbdd7252259368
Chris Kihlstrom <ckihlst@gmail.com>
2017-10-26 16:56:19 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    puts "Fizz"
+  if int % 15 == 0
+  "FizzBuzz"
+  elsif int % 3 == 0
+  "Fizz"
   elsif int % 5 == 0
-    puts "Buzz"
-  elsif int % 15 == 0
-    puts "FizzBuzz"
+    "Buzz"
   else
-    puts "Nil"
+    nil
   end
 end
