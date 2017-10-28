092545da34506265da3a6d1fcd7bf2e53d962dc0
Lisa Koperski <lisa_koperski_junk@yahoo.com>
2017-10-28 04:41:27 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,12 @@
-def fizzbuzz(int)
-   if int % 3 == 0
+fizz_int = gets.strip
+
+def fizzbuzz(fizz_int)
+   if fizz_int / 3 == 0
      puts "Fizz"
-  elsif int % 5 == 0
+  elsif fizz_int / 5 == 0
      puts "Buzz"
-  elsif int % 3 && 5 == 0
+  elsif (fizz_int / 3 && fizz_int / 5) == 0
      puts "FizzBuzz"
-  else int % !3 || !5 == 0
+  else (fizz_int / 3 || fizz_int / 5) == !0
      puts nil
 end
