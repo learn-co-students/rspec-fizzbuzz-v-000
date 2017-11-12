0df097f0e5216b0c5fe8313154ecc71321ad3262
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-12 02:39:45 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,3 @@
 def fizzbuzz(n)
-  p1 = n % 3 == 0 ? "Fizz" : ""
-  p2 = n % 5 == 0 ? "Buzz" : ""
-  !"#{p1}#{p2}".empty? ? "#{p1}#{p2}" : nil
+  n%15 == 0 ? 'FizzBuzz' : n%3 == 0 ? 'Fizz' : n%5 == 0 ? 'Buzz' : nil
 end
