cf39041cf0cc915c2f53985b429ccc6bc225d34f
Christopher DiStasio <cadistasio@gmail.com>
2017-11-20 16:34:40 -0600

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
-  elsif int % 3 == 0 # if the number int is divisible by 3
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
