73117e70fdbb2281d6fe2fdd97f018aedd3c0d01
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-23 01:12:46 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,21 @@
 def fizzbuzz(n)
   n%15 == 0 ? 'FizzBuzz' : n%3 == 0 ? 'Fizz' : n%5 == 0 ? 'Buzz' : nil
 end
+
+
+# def fizzbuzz(num)
+#   status = ""
+#   status += "Fizz" if (num % 3 == 0)
+#   status += "Buzz" if (num % 5 == 0)
+#   status.empty? ? nil : status
+# end
+
+# def fizzbuzz(num)
+#   if num % 15 == 0
+#     "FizzBuzz"
+#   elsif num % 3 == 0
+#     "Fizz"
+#   elsif num % 5 == 0
+#     "Buzz"
+#   end
+# end
