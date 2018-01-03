d0d54c87fd9ae42824336ad484bc8bf4def17a13
asyroyez <asyroyez@gmail.com>
2018-01-02 19:30:22 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,5 @@
 def fizzbuzz(n)
-  if n.modulo(3).zero? && n.modulo(5).zero? then return "FizzBuzz"
-  elsif n.modulo(3).zero? then return "Fizz"
-  elsif n.modulo(5).zero? then return "Buzz"
-  else return nil
-  end
+  n.modulo(3).zero? && n.modulo(5).zero? then return "FizzBuzz"
+  n.modulo(3).zero? then return "Fizz"
+  n.modulo(5).zero? then return "Buzz"
 end
