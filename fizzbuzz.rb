ff7b6702ea4274e95ea0cb52149e74aa96127d36
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-01-25 17:00:06 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,15 +2,16 @@ def fizzbuzz
 end
 
 def fizzbuzz(int)
-  if int % 3 == 0 
+  
+  if int % 3 == 0 && int % 5 == 0
+ "FizzBuzz"
+  
+  elsif int % 3 == 0 
    "Fizz"
 
-elsif int % 5 == 0 
+  elsif int % 5 == 0 
  "Buzz" 
  
- elsif int % 3 && 5 == 0
- "FizzBuzz"
-   
    end
  end
   
