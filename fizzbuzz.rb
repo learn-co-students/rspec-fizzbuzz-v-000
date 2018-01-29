c17db30325023e52196d5733a563ec81611523d0
Emily Welsch <emwelsch@gmail.com>
2018-01-29 20:34:21 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  end
+    puts "FizzBuzz"
   elsif int % 3 == 0
-    "Fizz"
-  end
+    puts "Fizz"
   elsif int % 5 == 0
-    "Buzz"
-  end
+    puts "Buzz"
   else
+    puts "nil"
   end
 end
