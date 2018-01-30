482f7ee9daa7e3caa1745c901a65b5de4011daf6
Igli Cekani <htnacekan@gmail.com>
2018-01-30 23:37:59 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(int)
 
-if int%3==0
-    "Fizz"
+if int%5==0 && int%3==0
+    "FizzBuzz"
 elsif int%5==0
     "Buzz"
-end
-
-if int%5==0 && int%3==0
-  "FizzBuzz"
+elsif int%3==0
+  "Fizz"
+else
+  return nil
 end
 end
