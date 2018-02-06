f149205354b24b5b6e08efd59ef0efb2f331e46c
Rima Jean <rima.jean@gmail.com>
2018-02-06 23:16:08 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz
-
-end 
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 3 == 0
+    "Fizz"
+  else
+    puts int
+  end
+end
