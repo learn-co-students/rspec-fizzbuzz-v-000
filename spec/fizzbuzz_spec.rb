5d51f0908a72dd30ca05166057db4ec6c8be25f0
Jamal Gelin <gelin.jamal@gmail.com>
2018-02-18 00:00:41 +0000

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,14 +1,14 @@
 require_relative './spec_helper.rb'
 
-def fizzBuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    return 'FizzBuzz'
+  elsif int % 3 == 0
+    return 'Fizz'
+  elsif int % 5 ==  0
+    return 'Buzz'
   else
-    number
+    return nil
   end
 end
 
