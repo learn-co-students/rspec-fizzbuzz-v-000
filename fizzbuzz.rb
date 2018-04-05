00c6aa5fa92e1c8ed767109cc8980dddb5c33ad5
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-04-05 22:20:59 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(number)
+ if number % 3 == 0 && number % 5 == 0
+   fizz_15 = "FizzBuzz"
+   elsif number % 3 == 0
+   fizz_3 = "Fizz"
+   elsif number % 5 == 0
+   fizz_5 = "Buzz"
+ end
+end
