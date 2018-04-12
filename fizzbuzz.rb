02403c4a5f173ed052c8ae4c78d8074b1b49008f
Dennis Lord <lorddennis1006@gmail.com>
2018-04-12 20:32:39 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+   return "FizzBuzz"
+  end
+
+  if num % 5 == 0
+   return "Buzz"
+  end
+
+  if num % 3 == 0
+   return "Fizz"
+  end
+
+end
