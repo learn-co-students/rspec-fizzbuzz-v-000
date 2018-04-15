6572f3be008c6547928d68d49f8fc77e371f9b10
Kai Ransome <ransomekai@gmail.com>
2018-04-15 01:40:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(int)
-  if int / 3 == 1
+  if int % 3 == 0
     return "Fizz"
-  elsif int / 5 == 1
+  elsif int % 5 == 0
     return "Buzz"
   elsif int % 15 == 0
     return "FizzBuzz"
