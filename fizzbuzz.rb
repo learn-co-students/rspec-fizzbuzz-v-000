81c067819fc422b893e4febdf48870908c0a7701
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-11 17:24:10 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  elsif int % 3 == 0
-    "Fizz"
-  elsif int % 5 == 0
-    "Buzz"
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+     "FizzBuzz"
+  elsif number % 3 == 0
+     "Fizz"
+   elsif number % 5 == 0
+     "Buzz"
+     
   end
 end
