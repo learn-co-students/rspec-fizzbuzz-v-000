5ee593814183f0bfbc4778d857d668200b96af4d
Richard Sanchez <richard.charles.s@gmail.com>
2018-05-31 23:44:56 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuzz(x)
+  if x % 3 == 0 && x % 5 != 0
+     "Fizz"
+  elsif x % 5 == 0 && x % 3 != 0
+     "Buzz"
+  elsif x % 5 == 0 && x % 3 == 0
+     "FizzBuzz"
+  else nil
+  end
+end
