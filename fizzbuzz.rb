2b9bea4b004fb3cbcaf6801e081e79b00f98b0b2
Jennifer Hong <jennifer.hong13@gmail.com>
2018-06-06 20:28:03 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 0 && int % 5 == 0
+  "FizzBuzz"
+  elsif int % 3 == 0 && int % 5 !=0
     "Fizz"
-  elsif int % 5 == 0
+  elsif int % 3 != 0 && int % 5 == 0
     "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  elsif int % 4 == 0
-    "nil"
+  else nil
   end
 end
