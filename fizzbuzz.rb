e4f8c60cd052d42e0e4aca6829a1a9e73db0edcc
Ana Bebic <anabebic.x@gmail.com>
2018-06-11 12:28:40 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,2 @@
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0 
-  "FizzBuzz"
-  
-  elsif (int % 3 == 0)
-  "Fizz"
-  
-  elsif (int % 5 == 0)
-  "Buzz"
-  
-  else 
-    nill
-  end
-  
-end
 
 
