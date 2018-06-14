28faa022c52d2d13a5903dd6262a7d75e607816a
Michael Blandford <mdblandford@gmail.com>
2018-06-14 16:01:57 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,11 @@
-def fizzbuzz
-end
-
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
+  if int % 15 == 0
     "FizzBuzz"
   elsif int % 5 == 0
-    "Buzz"    
+    "Buzz"
   elsif int % 3 == 0
     "Fizz"
   else
-     puts
-  end 
+     nil
+  end
 end
