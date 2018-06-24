d2d5a588a24f6e35de62cb41a7ddcb13579f3f4f
kitte anderson <kitteanderson@gmail.com>
2018-06-24 15:33:10 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,12 +5,12 @@
 
 def fizzbuzz(int)
   if (int % 3) == 0 && (int % 5) == 0
-       return "FizzBuzz"
-  elsif (int % 5) == 0
-       return "Buzz"
+    return "FizzBuzz"
   elsif (int % 3) == 0
     return "Fizz"
+  elsif (int % 5) == 0
+    return "Buzz"
   else
     return nil
-  end
+  end  
 end
