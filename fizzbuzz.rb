2120861df8c363f3568973fc20a75362d0bd0968
David McGrann <dpmcgrann@gmail.com>
2018-07-24 20:02:29 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 != 0
-    return "Fizz"
-  elsif int % 5 == 0 && int % 3 != 0
-    return "Buzz"
-  elsif int % 3 && int % 5 == 0
-    return "FizzBuzz"
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
   end
 end
