0c32b9df861f59247ce7c8e2593635e86033c915
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-08-03 20:27:39 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 != 0
-    return "Fizz"
-  elsif num % 5 == 0 && num % 3 != 0
-    return "Buzz"
-  elsif num % 3 == 0 && num % 5 == 0
+  if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
   else
     return nil
   end
