3e4620e79093f43b2eeb8dc56f55355d86cc8cfb
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-06 18:17:45 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,27 +1,19 @@
-require_relative './spec_helper.rb'
 
-def fizzbuzz(3)
-  if divisible by 3 = TRUE
-  puts = "Fizz"
-else 
-  puts = "nil"
-end
 
-def fizzbuzz(5)
-  if divisible by 5 = TRUE
-  puts = "Buzz"
-else 
+def fizzbuzz(int)
+  if int % 3 == 0
+  puts "Fizz"
+
+elsif int % 5 == 0
+puts "Buzz"
+
+elsif int % 3 && 5 == 0
+puts "FizzBuzz"
+
+else
   puts "nil"
 end
-
-def fizzbuzz(15)
-if divisible by 3 && 5 = TRUE
-puts = "FizzBuzz"
 end
 
-def fizzbuzz(4)
-  if not divisible by 3 or 5
-    puts "nil"
-  end
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
