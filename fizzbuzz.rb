1b275b0edf23b95ef11bc23983bc1913c69a9990
Tim Knoll <timothy.knoll@gmail.com>
2018-08-06 03:25:20 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz (num)
+# We expect fizzbuzz(15) to return "FizzBuzz"
+  if (num % 3 == 0 && num % 5 == 0)
+    puts "FizzBuzz"
+  end
+# We expect fizzbuzz(3) to return "Fizz"
+  if (num % 3 == 0)
+    puts "Fizz"
+  end
+# We expect fizzbuzz(5) to return "Buzz"
+  if (num % 5 == 0)
+    puts "Buzz"
+  end
+# return nil number not divisible
+  else
+    puts nil
+  end
+end
