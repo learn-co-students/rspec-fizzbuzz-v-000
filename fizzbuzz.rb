c5dad1b27f8e8465effdc153c59756cb621e9788
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-07 22:13:06 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,28 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+=begin
+def fizzbuzz(number)
+  if number % 5 == 0 && number % 3 == 0
+    return "FizzBuzz"
+  elseif number % 3 == 0
+    return "Fizz"
+  elseif number % 5 == 0
+    return "Buzz"
+  else
+    return nil
+  end
+end
+=end 
+
+def fizzbuzz(number)
+  if number % 5 == 0 && number % 3 == 0
+    return "FizzBuzz"
+  end
+  if number % 3 == 0
+    return "Fizz"
+  end
+  if number % 5 == 0
+    return "Buzz"
+  end
+end
