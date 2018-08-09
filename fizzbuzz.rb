442e995b2160aeab89d62c6dea4351e9f4e3f6d8
hugo valois <hvgaming@yahoo.com>
2018-08-09 19:35:01 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+
+
+def fizzbuzz(int)
+  if int % 3 == 0 
+    puts "Fizz"
+  
+  elsif int % 5 == 0 
+    puts "Buzz"
+
+  elsif int 15 % == 0
+    puts "Fizzbuzz"
+    
+  else int % 4 == 0
+    puts nil
+  end
+end
