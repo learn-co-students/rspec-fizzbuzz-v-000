42b0c4a2351b9484385d8276e7a1a3533ca410fd
Stef Claus <stef.claus@gmail.com>
2018-08-23 01:03:01 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
 def fizzbuzz(int)
-  if int % 15 == 0 
-    puts "Fizzbuzz"
-  elsif int % 5 == 0 
-    puts "Buzz"
-  else int % 3 == 0 
-    puts "Fizz"
-end
+  if int % 3 == 0 && int % 5 == 0 
+       "FizzBuzz"
+   elsif int % 5 == 0 
+     "Buzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  else 
+    
+    end
+  end
+ 
