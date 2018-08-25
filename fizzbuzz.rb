0cc07aacc53b81c6490e4a975e488fd94ffd06fb
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-08-25 18:47:00 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(int)
-if int % 3 == 0 && int % 5 == 0
+def fizzbuzz(num)
+  if num % 15 == 0
     "FizzBuzz"
-elsif int % 3 == 0
+  elsif num % 3 == 0
     "Fizz"
-elsif int % 5 == 0
+  elsif num % 5 == 0
     "Buzz"
-else int % 4 == 0
+  else
     nil
   end
 end
