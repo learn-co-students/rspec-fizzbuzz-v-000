32fbda07e05e725cec28e527a145223ae04bceb2
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-08 14:50:13 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,3 +7,14 @@ def fizzbuzz (int)
     "Buzz"
   end
 end
+
+
+#def fizzbuzz(int)
+  #if int % 3 == 0 && int % 5 == 0
+    #"FizzBuzz"
+  #elsif int % 3 == 0
+    #"Fizz"
+  #elsif int % 5 == 0
+    #"Buzz"
+  #end
+#end
