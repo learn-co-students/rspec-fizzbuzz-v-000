9674fc7f6857eea8d8e99a6c1f47efc36f8bfaa1
Ryan Brady <ryanbrady369@gmail.com>
2018-09-11 20:58:39 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  if num % 3 == 0
+    if num % 5 == 0
+      return "FizzBuzz"
+    end
+    return "Fizz"
+  end
+  if num % 5 == 0
+    return "Buzz"
+  end
+  nil
+end
