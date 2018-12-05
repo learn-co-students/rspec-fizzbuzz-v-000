a89dc36a1dbac7c68c4e1fbac090c1fa2cdf60c2
Sarah Paz <sarah.paz@gmail.com>
2018-12-05 03:11:29 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,15 +2,17 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3 and 5
-    "FizzBuzz" # Go FizzBuzz
-  elsif int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go Fizz
-  elsif int % 5 == 0 # if the number int is divisible by 5
-    "Buzz" # Go Buzz
-  end
+    if int % 3 == 0 && int % 5 == 0
+      "FizzBuzz"
+    elsif int % 5 == 0
+      "Buzz"
+    elsif int % 3 == 0
+      "Fizz"
+    else
+      nil
+    end
 end
 
-fizzbuzz(3) # => You should see a return of "Fizz"
-fizzbuzz(5) # => You should see a return of "Buzz"
-fizzbuzz(15)  # => You should see a return of "FizzBuzz"
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
