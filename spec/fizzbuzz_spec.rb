ecbe994f6e1eddfcf3499f8cb1b6ecc88af4e431
Ellen Pu <ellenpu251@gmail.com>
2019-01-15 07:22:23 +0000

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,15 @@
 require_relative './spec_helper.rb'
-# We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
+require_relative '../fizzbuzz.rb'
+# We are missing a 'require_relative' here. Check README.md for instructions and an explanation!
+def fizzbuzz(int)
+  if (int % 3 == 0 && int % 5 == 0)
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
+end
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
