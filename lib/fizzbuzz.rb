85d5db6a235a34ca4fc8c6a9c022a502dee208b6
Isaiah Magett <imagett10@gmail.com>
2019-01-18 23:50:16 +0000

    Done.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -13,9 +13,9 @@ end
 # my code
 # def fizzbuzz(user_input)
   user_input = gets.strip
-fizzbuzz(user_input/3) ="Fizz"
-fizzbuzz(user_input/5) ="Buzz"
-fizzbuzz(user_input/15) ="Fizzbuzz"
+#fizzbuzz(user_input/3) ="Fizz"
+#fizzbuzz(user_input/5) ="Buzz"
+# fizzbuzz(user_input/15) ="Fizzbuzz"
 # end
 
 
