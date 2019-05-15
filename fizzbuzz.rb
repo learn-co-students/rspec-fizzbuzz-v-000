8b54f8ba9c42b573d854574fac88bf1a2e97b0e6
CalebKnox <knoxcaleb@aol.com>
2019-05-15 13:50:42 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number.modulo(3).zero? and number.modulo(5).zero?
+    "FizzBuzz"
+  elsif number.modulo(5).zero?
+    "Buzz"
+  elsif number.modulo(3).zero?
+    "Fizz"
+  else
+    phrase = nil
+  end
+end
