4d20c98b7ddf2b63c221184e1ac7d12463082d9f
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-04 15:59:25 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,40 @@
-#def fizzbuzz(i)
 
+def fizzbuzz (i)
+  if i % 3 == 0 && i % 5 == 0
+    "FizzBuzz"
+  elsif i % 5 == 0
+    "Buzz"
+  elsif i % 3 == 0
+    "Fizz"
+  else
+    nil
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+#
+# def fizzbuzz(i)
+#
 #  if i % 15 == 0
+#    #here the 15 is the least common multiplier (LCM). meaning that if the i is divisible by both 3 and 5 it is divisible by 15
 #  "FizzBuzz"
 #  elsif i % 3 == 0
 #  "Fizz"
 #  elsif i % 5 == 0
 #  "Buzz"
-
-#else !(i % 3 == 0 || i % 5 == 0)
-#  nil
-
-#  end
 #
-
-Dont know what to do
-  
-end
+# else !(i % 3 == 0 || i % 5 == 0)
+#  nil
+#
+#  end
+# #
