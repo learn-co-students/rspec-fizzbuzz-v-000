4ba10ed1fb81c6448ebe498e822ed29014000eb1
Jonathan Bernstein <jjbernstein@gmail.com>
2020-09-10 23:44:25 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(int)
+  if int % 15 == 0
+    return "FizzBuzz"
+  end
+  if int % 5 == 0
+   return "Buzz"
+ end
+  if int % 3 == 0
+     return "Fizz"
+  end
+     nil
+    
+
+
+end
